22e64422b73092510772665429503665fcc84e2f
Pankaj singh <88103744+pankajsinghveersatech@users.noreply.github.com>
2025-06-17 20:23:07 +0530

    Update Footer.tsx

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -24,7 +24,7 @@ export default function Footer() {
           <Typography color="grey.700" variant="h6">
             Developed by{" "}
             <Link
-              href="https://github.com/crazy-man22"
+              href="https://github.com/pankajsinghveersatech"
               underline="none"
               sx={{ color: "text.primary" }}
               target="_blank"
